63000972642f4a07351ed823f3c10745282bd335
ReGitnald <rverrier01@manhattan.edu>
2026-01-15 22:17:07 -0500

    2026 Finishing this project

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,34 @@ class LeafNode(HTMLNode):
         return html_tag
     
 class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+
+def text_node_to_html_node(text_node):
+    if text_type := text_node.text_type == 'NORMAL':
+        return LeafNode(tag=None, value=text_node.text)
+    elif text_type == 'BOLD':
+        return LeafNode(tag='b', value=text_node.text)
+    elif text_type == 'ITALICS':
+        return LeafNode(tag='i', value=text_node.text)
+    elif text_type == 'CODE':
+        return LeafNode(tag='code', value=text_node.text)
+    elif text_type == 'LINK':
+        return LeafNode(tag='a', value=text_node.text, props={'href': text_node.url})
+    elif text_type == 'IMAGE':
+        return LeafNode(tag='img', value='', props={'src': text_node.url, 'alt': text_node.text})
+    else:
+        raise ValueError(f"Unknown text type: {text_node.text_type}")
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -14,6 +14,11 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node3)
         self.assertNotEqual(node2, node4)
         self.assertNotEqual(node, node5)
+def test_text(self):
+    node = TextNode("This is a text node", TextType.TEXT)
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, None)
+    self.assertEqual(html_node.value, "This is a text node")
 
 
 
